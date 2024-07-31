babe1fa3b9c0e6a39391f2dbdc005988d12b3185
17dshaikh <95411076+17dshaikh@users.noreply.github.com>
2024-07-31 14:05:11 +0100

    Update Locomotion.py

--- Code/Locomotion.py
+++ Code/Locomotion.py
@@ -130,3 +130,150 @@ class Locomotion:
             Positive values rotate right, negative values rotate left. Should be in the range -1.0 - 1.0.
         """
         self._rotation_factor = factor*100
+
+def set_lean(self, lean):
+        """Set the distance that it should lean left to right.
+
+        Parameters
+        ----------
+        lean : float
+            Positive values lean right, negative values lean left. Should be in the range -1.0 - 1.0.
+        """
+        self._lean_factor = lean*20
+
+    def set_height_offset(self, height):
+        """Set the extra distance for the chassis to be off the ground.
+
+        Parameters
+        ----------
+        height : float
+            Should be in the range 0.0-1.0.
+        """
+        self._height_factor = height*40
+
+    def toggle_standing(self):
+        """Toggle the standing keyframes."""
+        self._standing = not self._standing
+
+    def Shutdown(self):
+        """Shutdown and stop the Run loop."""
+        self._running = False
+
+    def _InterpolateKeyframes(self, ratio):
+        """Interpolate between the current and next keyframes for each leg and apply the servo positions.
+
+        Parameters
+        ----------
+        ratio : float
+            The ratio of each keyframe in the interpolation. Should be in the range 0.0-1.0.
+        """
+        keyframes = self._keyframes[self.Leg.FrontRight.value]
+        foot, leg, shoulder = KeyframeKinematics([keyframes[0][0], keyframes[0][1]+self._height_factor, keyframes[0][2]-self._lean_factor],
+                                                 [keyframes[1][0], keyframes[1][1]+self._height_factor, keyframes[1][2]-self._lean_factor],
+                                                 ratio)
+        self._FrontRightLeg(foot, leg, shoulder)
+
+        keyframes = self._keyframes[self.Leg.BackLeft.value]
+        foot, leg, shoulder = KeyframeKinematics([keyframes[0][0], keyframes[0][1]+self._height_factor, keyframes[0][2]+self._lean_factor],
+                                                 [keyframes[1][0], keyframes[1][1]+self._height_factor, keyframes[1][2]+self._lean_factor],
+                                                 ratio)
+        self._BackLeftLeg(foot, leg, shoulder)
+
+        keyframes = self._keyframes[self.Leg.FrontLeft.value]
+        foot, leg, shoulder = KeyframeKinematics([keyframes[0][0], keyframes[0][1]+self._height_factor, keyframes[0][2]+self._lean_factor],
+                                                 [keyframes[1][0], keyframes[1][1]+self._height_factor, keyframes[1][2]+self._lean_factor],
+                                                 ratio)
+        self._FrontLeftLeg(foot, leg, shoulder)
+        
+        keyframes = self._keyframes[self.Leg.BackRight.value]
+        foot, leg, shoulder = KeyframeKinematics([keyframes[0][0], keyframes[0][1]+self._height_factor, keyframes[0][2]-self._lean_factor],
+                                                 [keyframes[1][0], keyframes[1][1]+self._height_factor, keyframes[1][2]+self._lean_factor],
+                                                 ratio)
+        self._BackRightLeg(foot, leg, shoulder)
+    
+    def _FrontRightLeg(self, foot, leg, shoulder):
+        """Helper function for setting servo angles for the front right leg.
+
+        Parameters
+        ----------
+        foot : float
+            Servo angle for foot in degrees.
+        leg : float
+            Servo angle for leg in degrees.
+        shoulder : float
+            Servo angle for shoulder in degrees.
+        """
+        self._servos[2].set_angle(180-foot)
+        self._servos[1].set_angle(180-(leg+90))
+        self._servos[0].set_angle(shoulder)
+    
+    def _FrontLeftLeg(self, foot, leg, shoulder):
+        """Helper function for setting servo angles for the front left leg.
+
+        Parameters
+        ----------
+        foot : float
+            Servo angle for foot in degrees.
+        leg : float
+            Servo angle for leg in degrees.
+        shoulder : float
+            Servo angle for shoulder in degrees.
+        """
+        self._servos[5].set_angle(foot)
+        self._servos[4].set_angle(180-(90-leg))
+        self._servos[3].set_angle(180-shoulder)
+    
+    def _BackRightLeg(self, foot, leg, shoulder):
+        """Helper function for setting servo angles for the back right leg.
+
+        Parameters
+        ----------
+        foot : float
+            Servo angle for foot in degrees.
+        leg : float
+            Servo angle for leg in degrees.
+        shoulder : float
+            Servo angle for shoulder in degrees.
+        """
+        self._servos[8].set_angle(180-foot)
+        self._servos[7].set_angle(180-(leg+90))
+        self._servos[6].set_angle(shoulder)
+    
+    def _BackLeftLeg(self, foot, leg, shoulder):
+        """Helper function for setting servo angles for the back left leg.
+
+        Parameters
+        ----------
+        foot : float
+            Servo angle for foot in degrees.
+        leg : float
+            Servo angle for leg in degrees.
+        shoulder : float
+            Servo angle for shoulder in degrees.
+        """
+        self._servos[11].set_angle(foot)
+        self._servos[10].set_angle(180-(90-leg))
+        self._servos[9].set_angle(180-shoulder)
+
+    def _shift_keyframes(self, ratio = 0):
+        """Shift the next keyframe to be the current keyframe.
+        
+        Parameters
+        ----------
+        ratio : float
+            Ratio for interpolating keyframes if shifting between keyframe transitions.
+        """
+        if ratio > 0:
+            for i in range(len(self._keyframes)):
+                key1 = self._keyframes[i][0]
+                key2 = self._keyframes[i][1]
+                self._keyframes[i][0] = [interpolate(key1[0], key2[0], ratio), interpolate(key1[1], key2[1], ratio), interpolate(key1[2], key2[2], ratio)]
+        else:
+            for i in range(len(self._keyframes)):
+                self._keyframes[i][0] = self._keyframes[i][1]
+
+    def _set_standing_keyframes(self):
+        """Helper function for seting a static standing keyframe"""
+        for i in range(4):
+            self._keyframes[i][1] = [0, 150, 40]
+     
